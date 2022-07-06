316e5697a8a43a3d66457baa4d015b33298d27c6
Danilo Moraes <d169649@dac.unicamp.br>
2022-07-06 02:25:45 -0300

    Test SD Card and QRS Detect

--- NEW_QRS/NEW_QRS.ino
+++ NEW_QRS/NEW_QRS.ino
@@ -1,6 +1,6 @@
 #include <SPI.h>
 #include "SD.h"
-
+#include "ResponsiveAnalogRead.h"
 
 #define SCK  18
 #define MISO  19
@@ -15,8 +15,8 @@
 */
 #define M            5    
 #define N           30 
-#define WINSIZE    150    
-#define N_SAMPLES  300
+#define WINSIZE    250    
+#define N_SAMPLES  1000
 #define MAXSIZE      6
 #define next(elem)   ((elem+1)%MAXSIZE)
 #define HP_CONSTANT  ((float) 1 / (float) M)
@@ -26,15 +26,13 @@
 // resolution of RNG
 #define RAND_RES 100000000
 
-
-
 const int ECG_PIN =  34;       // the number of the ECG pin (analog)
 const int LEADS_OFF_PLUS_PIN = 35;
 const int LEADS_OFF_MINUS_PIN = 32;
 const int SD_CS = 0;
 
 unsigned long bpm_buff[MAX_BPM_SIZE] = {0};
-int bpm_wd_ptr = 0;
+int bpm_wr_ptr = 0;
 int bpm_rd_ptr = 0;
 float bpm = 0;
 
@@ -59,10 +57,13 @@ int trig_time = 0 ;
 float win_max = 0 ;
 int win_idx = 0 ;
 
-int cprTimeRead1 = 0 ;
-int cprTimeRead2 = 0 ;
+int tmp = 0;
+
+int cprTimeRead_1 = 0 ;
+int cprTimeRead_2 = 0 ;
 int timeCPR = 0 ;
-float CPRSUM;
+float cprsum;
+int QRS = 0;
 
 unsigned long previousMicros  = 0;        // will store last time LED was updated
 unsigned long foundTimeMicros = 0;        // time at which last QRS was found
@@ -70,33 +71,36 @@ unsigned long old_foundTimeMicros = 0;        // time at which QRS before last w
 unsigned long currentMicros   = 0;        // current time
 const long PERIOD = 1000000 / WINSIZE;
 
+bool Plotting = true;
+int i = 0;
 int n_interat = 0;
+int interval = 0;
 
-const char* Filename = "Output.csv";
+const char* Filename = "/Output.csv";
 
 void writeFile(fs::FS &fs, const char * path, const char * message){
-    Serial.printf("Writing file: %s\n", path);
+    //Serial.printf("Writing file: %s\n", path);
 
     File file = fs.open(path, FILE_WRITE);
     if(!file){
-        Serial.println("Failed to open file for writing");
+       Serial.println("Failed to open file for writing");
         return;
     }
     if(file.print(message)){
-        Serial.println("File written");
+       Serial.println("File written");
     } else {
-        Serial.println("Write failed");
+       Serial.println("Write failed");
     }
     file.close();
 }
 
 void appendFile(fs::FS &fs, const char * path, const char * message){
-    Serial.printf("Appending to file: %s\n", path);
+    //Serial.printf("Appending to file: %s\n", path);
 
     File file = fs.open(path, FILE_APPEND);
     if(!file){
-        Serial.println("Failed to open file for appending");
-        return;
+       Serial.println("Failed to open file for appending");
+       return;
     }
     if(file.print(message)){
         Serial.println("Message appended");
@@ -106,12 +110,14 @@ void appendFile(fs::FS &fs, const char * path, const char * message){
     file.close();
 }
 
-boolean detect(float ecg_data)
-{
+boolean detect(float ecg_data){
+  
   int aux = 0;
   //Put a new data in the circular buffer before the bandpass filter. 
   ecg_raw_data[ecg_wr_ptr] = ecg_data;
   ecg_wr_ptr = (ecg_wr_ptr+1)%(M+1);
+
+  //Serial.println(ecg_raw_data[ecg_rd_ptr]);
   
   // Put a new data into the circular buffer of the High Pass filter. 
   // This step checks if the circular buffer is available for a new data. 
@@ -137,16 +143,15 @@ boolean detect(float ecg_data)
     hp_data1 = ecg_raw_data[aux];   
     hp_data2 = HP_CONSTANT * hp_sum;
     
-    hp_data[hp_wr_ptr] = hp_data1 - hp_data2;
-      
-  }
+    hp_data[hp_wr_ptr] = hp_data1 - hp_data2;      
+  } 
   ecg_rd_ptr = (ecg_rd_ptr+1)%(M+1);
   hp_wr_ptr = (hp_wr_ptr+1)%(N+1);
 
  //Low pass filtering 
   // shift in new sample from high pass filter
   lp_sum += hp_data[hp_rd_ptr] * hp_data[hp_rd_ptr];
-  
+
   if(n_interat < N){
     // first fill buffer with enough points for LP filter
     next_ptr = 0;
@@ -160,12 +165,12 @@ boolean detect(float ecg_data)
     lp_sum -= hp_data[aux] * hp_data[aux];
 
     next_ptr = lp_sum;
-  }
-  
+    
+  } 
   // done reading HP buffer, increment position
   hp_rd_ptr = (hp_rd_ptr+1)%(N+1);
-
-
+  //Serial.println(hp_data[hp_rd_ptr]);
+  
   /// Adapative thresholding beat detection
   // set initial threshold        
   if(n_interat < WINSIZE) {
@@ -187,7 +192,7 @@ boolean detect(float ecg_data)
       trig_time = 0;
     }
   }
-  
+  //Serial.println(next_ptr);
   // find if we have a new max
   if(next_ptr > win_max) 
     win_max = next_ptr;
@@ -195,7 +200,7 @@ boolean detect(float ecg_data)
   // find if we are above adaptive threshold
   if(next_ptr > treshold && !triggered) {
     triggered = true;
-
+    
     return true;
   }
         // else we'll finish the function before returning FALSE,
@@ -228,7 +233,6 @@ boolean detect(float ecg_data)
 
 void setup() {
   // set the digital pin as output:
-  int i =6;
   Serial.begin(115200);
   
   pinMode(ECG_PIN, INPUT);
@@ -250,35 +254,55 @@ void setup() {
     
   writeFile(SD,Filename,"Counter,Time,QRS");
 }
-bool Plotting = true;
-int i = 0;
 
 void loop() {
-  delay(2);
+  delay(10);
   String aux = "";
   currentMicros = micros();
   previousMicros = currentMicros;
 
   int next_ecg_pt = analogRead(ECG_PIN);
   boolean QRS_detected = false;
-  
+
+   if (currentMicros - previousMicros >= PERIOD) {
+      // save the last time you blinked the LED
+      previousMicros = currentMicros;
+   }
+
   //give next data point to algorithm
   QRS_detected = detect(next_ecg_pt);
-
-  if(i < N_SAMPLES){
+  Serial.println(QRS_detected);
+  
+   if(i < N_SAMPLES){
     aux += String(i);
     aux += ",";
     aux += String(currentMicros);
     aux += ",";
     aux += String(QRS_detected);
+    aux += ",";
+    aux += bpm_buff[bpm_rd_ptr];
     aux += "\r\n";
-    appendFile(SD, "/teste.csv", aux.c_str());
-    delay(200);
+    appendFile(SD, Filename, aux.c_str());
+    delay(20);
   }
-  i++;
-
-  if(QRS_detected == true)
-    foundTimeMicros = micros(); 
-
-}    
   
+  if(QRS_detected == true){
+    bpm_buff[bpm_wr_ptr] = (60.0 / (((float) (foundTimeMicros - old_foundTimeMicros)) / 1000000.0));
+    
+    bpm_wr_ptr = (bpm_wr_ptr+1)%MAX_BPM_SIZE;
+    
+    bpm += bpm_buff[bpm_rd_ptr];
+    
+    tmp = bpm_rd_ptr - MAX_BPM_SIZE + 1;
+    if(tmp < 0) 
+      tmp += MAX_BPM_SIZE;
+    
+    bpm -= bpm_buff[tmp];
+    
+    bpm_rd_ptr++;
+    bpm_rd_ptr %= MAX_BPM_SIZE;
+
+    old_foundTimeMicros = foundTimeMicros;      
+  }   
+  i++;
+}  
